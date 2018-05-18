e148c368f925a01fa3ce2eb3ffa7c2d606b2c94e
macmini <macmini@macminis-mac-mini.local>
2018-05-18 13:49:30 +0300

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by macmini on 18.05.2018.
+//  Copyright © 2018 macmini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
